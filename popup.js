8be53402270b77129e6340964da3801ad567eedd
1twodrei <118087221+1twodrei@users.noreply.github.com>
2023-12-04 18:46:05 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('saveButton').addEventListener('click', saveOldVersion);
+});
+
+function saveOldVersion() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'saveOldVersion' });
+  });
+}
+
